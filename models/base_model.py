1023b7db4b12608e40ef10f341f375f579c4a225
Armynerh <aimenah@outlook.com>
2022-08-09 10:51:27 +0100

    updated arguements

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,19 @@ from datetime import datetime
 class BaseModel:
     """public instances of class BaseModel"""
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor for the class"""
-        self.old_id = uuid.uuid4()
-        self.id= str(self.old_id)
-        self.created_at = datetime.now()
-        self.updated_at= datetime.now()
+        if len(kwargs) != 0:
+            self.id =kwargs['id']
+            self.created_at =datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f') 
+            self.created_at =datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else: 
+            self.old_id = uuid.uuid4()
+            self.id= str(self.old_id)
+            self.created_at = datetime.now()
+            self.updated_at= datetime.now()
+    
+            
     def __str__(self):
         """string repesentation """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
@@ -28,4 +35,5 @@ class BaseModel:
         dict_rep['created_at'] = self.created_at.isoformat()  
         dict_rep['updated_at'] = self.updated_at.isoformat()
         return dict_rep
+    
     
